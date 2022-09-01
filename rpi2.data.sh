53e4b6b587b9122400e1a14e15e04a3a31d251b4
sergeyshem <95234480+sergeyshem@users.noreply.github.com>
2022-09-01 11:29:28 -0500

    expanding time frame and adding tail

--- rpi2.data.sh
+++ rpi2.data.sh
@@ -16,5 +16,5 @@
 ##fi
 
 printf "[\n";
-curl  -s -H 'Content-Type: application/json' https://data.sagecontinuum.org/api/v1/query -d  '{"start":"-120s","filter":{"host":"'$2'.ws-rpi","vsn":"'$1'"}}';
+curl  -s -H 'Content-Type: application/json' https://data.sagecontinuum.org/api/v1/query -d  '{"start":"-180s","tail":1,"filter":{"host":"'$2'.ws-rpi","vsn":"'$1'"}}';
 printf "]\n";
