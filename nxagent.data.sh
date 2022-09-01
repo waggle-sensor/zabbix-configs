6d061c170a928413b66f154b3461d68329542221
sergeyshem <95234480+sergeyshem@users.noreply.github.com>
2022-09-01 11:25:28 -0500

    expanding time frame and adding tail

--- nxagent.data.sh
+++ nxagent.data.sh
@@ -2,10 +2,10 @@
 #vsn=$1                                                                                                                                                                                                    
 #host="ws-nxagent"                                                                                                                                                                                         
                                                                                                                                                                                                            
-printf "[\n";                                                                                                                                                                                              
-curl  -s -H 'Content-Type: application/json' https://data.sagecontinuum.org/api/v1/query -d  '{"start":"-120s","filter":{"host":"*.ws-nxagent","vsn":"'$1'"}}';                                            
-printf "]\n";                                                                                                                                                                                              
-                                                                                                                                                                                                           
+printf "[\n";
+curl  -s -H 'Content-Type: application/json' https://data.sagecontinuum.org/api/v1/query -d  '{"start":"-180s","tail":1,"filter":{"host":"*.ws-nxagent","vsn":"'$1'"}}';
+printf "]\n";
+
 ##a="";                                                                                                                                                                                                    
 ##a=`curl  -s -H 'Content-Type: application/json' https://data.sagecontinuum.org/api/v1/query -d  '{"start":"-120s","filter":{"host":"*.ws-nxagent","vsn":"'$1'"}}'`;                                      
 ##                                                                                                                                                                                                         
